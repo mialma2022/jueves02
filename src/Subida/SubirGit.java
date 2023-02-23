f078cd38fa6cec8b59f19149d16d20a03a09b87b
jumur <jumur@LAPTOP-ERN5QTED>
2023-02-23 13:34:02 +0100

    Mi primera copia respaldo

---
+++ src/Subida/SubirGit.java
@@ -0,0 +1,17 @@
+package Subida;
+
+// Practicar con git
+public class SubirGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Subida .1..........");	
+		System.out.println(" ......... hola pablo     ...........");	
+		System.out.println("Subida ..2.........");	
+		System.out.println("Subida ....3.......");
+		System.out.println("Subida ....4.......");
+		System.out.println("Subida ....5 .......");
+
+	}
+
+}
